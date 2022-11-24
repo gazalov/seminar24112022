05682ee4b49a6aacf1f4ce3451891b01e289df81
a.gazalov <gazalov@list.ru>
2022-11-24 21:26:34 +0300

    Task 3

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int what = new Random().Next(10, 100);
+int deci = what / 10;
+int x = what % 10;
+int result;
+if(deci>x) 
+{
+    result = deci;
+}
+else
+{
+    result = x;
+}
+Console.WriteLine($"Рандомное число: {what}");
+Console.WriteLine(result);
---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число 1 ");
+int x = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число 2 ");
+int y = int.Parse(Console.ReadLine());
+
+if(x % y == 0)
+{
+    Console.WriteLine("Число 2 кратно числу 1");
+}
+else
+{
+    Console.WriteLine(x % y);
+}
